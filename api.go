f080c8b29d0507ad49f002f3675b76c18b7ea141
santtuniskanen <santtuniska02@gmail.com>
2024-02-29 21:21:12 +0200

    updated gitignore and api

--- api.go
+++ api.go
@@ -43,6 +43,7 @@ func (s *APIServer) Run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccount))
 
 	log.Println("JSON API server running on port: ", s.listenAddr)
 	http.ListenAndServe(s.listenAddr, router)
@@ -63,8 +64,11 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error 
 }
 
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	account := NewAccount("Santtu", "Niskanen")
-	return WriteJSON(w, http.StatusOK, account)
+	id := mux.Vars(r)["id"]
+
+	fmt.Println(id)
+	
+	return WriteJSON(w, http.StatusOK, &Account{})
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
